e4cebc8bb9b7831ac69e1754aa8054f4eeff2405
devnaroopesh01 <devnaroopesh01@gmail.com>
2026-02-17 14:29:33 +0530

    UC1: Print OOPS implemented

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,19 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print OOPS to console
+ * @author Devna
+ * @version 1.0
+ */
+
 public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of application
+     */
     public static void main(String[] args) {
+
+        // Print OOPS
         System.out.println("OOPS");
+
     }
 }
