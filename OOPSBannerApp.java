0f98382c5be1293b1c70da6871b322a026982cbf
devnaroopesh01 <devnaroopesh01@gmail.com>
2026-02-17 15:14:54 +0530

    UC2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,21 @@
 /**
  * OOPSBannerApp
- * UC1: Print OOPS to console
+ * UC2: Print OOPS Banner using asterisks
  * @author Devna
- * @version 1.0
+ * @version 2.0
  */
 
 public class OOPSBannerApp {
 
-    /**
-     * Main method - Entry point of application
-     */
     public static void main(String[] args) {
 
-        // Print OOPS
-        System.out.println("OOPS");
+        System.out.println(" *****   *****   ******   *****");
+        System.out.println("*     * *     *  *     * *");
+        System.out.println("*     * *     *  *     * *");
+        System.out.println("*     * *     *  ******   *****");
+        System.out.println("*     * *     *  *             *");
+        System.out.println("*     * *     *  *             *");
+        System.out.println(" *****   *****   *       *****");
 
     }
 }
